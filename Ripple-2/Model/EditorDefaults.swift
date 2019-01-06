efe7e7c95c4a077c2e2654f0ec41cb0182987e95
Tim Ng <timothyng2008@me.com>
2019-01-06 10:44:01 +0800

    feat: Add Editor Defaults for placeholder

---
+++ Ripple-2/Model/EditorDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  EditorDefaults.swift
+//  Ripple-2
+//
+//  Created by Tim Ng on 1/6/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import Foundation
+
+struct EditorDefaults {
+    static let placeholderText = "Write a thank you message to someone!"
+}
