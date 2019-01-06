c17b513019d74840807356bc9e98e1db03a70a65
Tim Ng <timothyng2008@me.com>
2019-01-06 10:22:50 +0800

    feat: Add page model

---
+++ Ripple-2/Model/Page.swift
@@ -0,0 +1,16 @@
+//
+//  Page.swift
+//  Ripple-2
+//
+//  Created by Tim Ng on 1/6/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    var imageName: String
+    var headerText: String
+    var bodyText: String
+}
+
