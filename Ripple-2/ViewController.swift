e74c98bbd60f058c16f9441f39705e3a72a6427b
Tim Ng <timng@tims-mbp.mynet>
2019-01-06 10:08:55 +0800

    Initial Commit

---
+++ Ripple-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ripple-2
+//
+//  Created by Tim Ng on 1/6/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
