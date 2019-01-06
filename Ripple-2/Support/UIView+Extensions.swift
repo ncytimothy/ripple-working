c31ccb3ef44ff6914114236e726767eb7fabd707
Tim Ng <timothyng2008@me.com>
2019-01-06 14:53:41 +0800

    feat: Add UIView extensions

---
+++ Ripple-2/Support/UIView+Extensions.swift
@@ -0,0 +1,72 @@
+//
+//  UIView+Extensions.swift
+//  Ripple-2
+//
+//  Created by Tim Ng on 1/6/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addConstraintsWith(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+            
+            
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
+        
+    }
+    
+    static func setHeaderContainerView() -> UIView {
+        let containterView = UIView()
+        containterView.translatesAutoresizingMaskIntoConstraints = false
+        return containterView
+    }
+}
+
+extension UITextView {
+    func setHeaderTextView(title: String, subtitle: String) -> UITextView {
+        let textView = UITextView()
+        
+        textView.backgroundColor = .green
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 32), NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        attributedText.append(NSAttributedString(string: "\n\(subtitle)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 22), NSAttributedString.Key.foregroundColor: UIColor.white]))
+        
+        textView.attributedText = attributedText
+        
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textView
+    }
+    
+}
+
+extension UILabel {
+    static func setHeaderLabel(title: String, subtitle: String) -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 32), NSAttributedString.Key.foregroundColor: UIColor.white])
+        
+        attributedText.append(NSAttributedString(string: "\n\(subtitle)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 22), NSAttributedString.Key.foregroundColor: UIColor.white]))
+        
+        label.attributedText = attributedText
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+        
+    }
+    
+}
+
